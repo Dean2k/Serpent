15335eb6ba8586bedb056c67ea4c7a71eed35767
Shrek <dean2k@live.co.uk>
2022-07-19 23:38:27 +0100

    Updated credits.

--- ReModCE/Components/ItemOrbitPlayerComponent.cs
+++ ReModCE/Components/ItemOrbitPlayerComponent.cs
@@ -23,7 +23,6 @@ namespace Serpent.Components
             var userInfoTransform = VRCUiManagerEx.Instance.MenuContent().transform.Find("Screens/UserInfo");
             var buttonContainer = userInfoTransform.Find("Buttons/RightSideButtons/RightUpperButtonColumn/");
             targetMenu.AddButton("Item Orbit", "Makes all Pickups spin around the targeted player.", ItemOrbit, ResourceManager.GetSprite("remodce.link"));
-            targetMenu.AddButton("Swastika Orbit", "Makes a Swastika around the targeted player.", ItemSwastika, ResourceManager.GetSprite("remodce.link"));
 
         }
 
@@ -98,72 +97,6 @@ namespace Serpent.Components
                     vrc_Pickup.transform.position = target.transform.position + target.transform.forward * 1.5f;
                     target.transform.Rotate(new Vector3(0f, 380 / vrc_Pickups.Length, 0f));
                 }
-            }
-            if (ItemOrbitSwastikaPlayerEnabled)
-            {
-                VRCPlayer player = null;
-                try
-                {
-                    player = PlayerManager.field_Private_Static_PlayerManager_0.GetPlayer(targetPlayer.prop_String_0)
-                        ._vrcplayer;
-                }
-                catch
-                {
-                    ItemSwastika();
-                }
-
-                var transform = player.transform;
-                var playerPosition = transform.position;
-
-                try
-                {
-                    for (int i = 0; i < vrc_Pickups.Length; i++)
-                    {
-                        VRC_Pickup vrc_Pickup = vrc_Pickups[i];
-                        if (Networking.GetOwner(vrc_Pickup.gameObject) != Networking.LocalPlayer)
-                        {
-                            Networking.SetOwner(Networking.LocalPlayer, vrc_Pickup.gameObject);
-                        }
-                    }
-                    vrc_Pickups[0].transform.position = new Vector3(playerPosition.x, playerPosition.y, playerPosition.z);
-                    vrc_Pickups[1].transform.position = new Vector3(playerPosition.x, playerPosition.y + 0.5f, playerPosition.z);
-                    vrc_Pickups[2].transform.position = new Vector3(playerPosition.x, playerPosition.y + 1f, playerPosition.z);
-                    vrc_Pickups[3].transform.position = new Vector3(playerPosition.x, playerPosition.y + 1.5f, playerPosition.z);
-                    vrc_Pickups[4].transform.position = new Vector3(playerPosition.x, playerPosition.y + 2f, playerPosition.z);
-                    vrc_Pickups[17].transform.position = new Vector3(playerPosition.x, playerPosition.y + 0.5f, playerPosition.z);
-                    vrc_Pickups[18].transform.position = new Vector3(playerPosition.x, playerPosition.y + 1f, playerPosition.z);
-                    vrc_Pickups[19].transform.position = new Vector3(playerPosition.x, playerPosition.y + 1.5f, playerPosition.z);
-                    vrc_Pickups[20].transform.position = new Vector3(playerPosition.x, playerPosition.y, playerPosition.z);
-                    vrc_Pickups[21].transform.position = new Vector3(playerPosition.x, playerPosition.y + 2.5f, playerPosition.z);
-                    vrc_Pickups[22].transform.position = new Vector3(playerPosition.x, playerPosition.y + 3f, playerPosition.z);
-
-
-
-                    vrc_Pickups[5].transform.position = new Vector3(playerPosition.x + 0.5f, playerPosition.y + 1.5f, playerPosition.z);
-                    vrc_Pickups[6].transform.position = new Vector3(playerPosition.x + 1f, playerPosition.y + 1.5f, playerPosition.z);
-                    vrc_Pickups[7].transform.position = new Vector3(playerPosition.x - 0.5f, playerPosition.y + 1.5f, playerPosition.z);
-                    vrc_Pickups[8].transform.position = new Vector3(playerPosition.x - 1f, playerPosition.y + 1.5f, playerPosition.z);
-                    vrc_Pickups[27].transform.position = new Vector3(playerPosition.x + 1.5f, playerPosition.y + 1.5f, playerPosition.z);
-                    vrc_Pickups[28].transform.position = new Vector3(playerPosition.x - 1.5f, playerPosition.y + 1.5f, playerPosition.z);
-
-                    vrc_Pickups[9].transform.position = new Vector3(playerPosition.x + 1.5f, playerPosition.y + 1.7f, playerPosition.z);
-                    vrc_Pickups[10].transform.position = new Vector3(playerPosition.x + 1.5f, playerPosition.y + 1.9f, playerPosition.z);
-                    vrc_Pickups[23].transform.position = new Vector3(playerPosition.x + 1.5f, playerPosition.y + 2.1f, playerPosition.z);
-
-                    vrc_Pickups[11].transform.position = new Vector3(playerPosition.x - 1.5f, playerPosition.y + 1.3f, playerPosition.z);
-                    vrc_Pickups[12].transform.position = new Vector3(playerPosition.x - 1.5f, playerPosition.y + 1.1f, playerPosition.z);
-                    vrc_Pickups[24].transform.position = new Vector3(playerPosition.x - 1.5f, playerPosition.y + 1.3f, playerPosition.z);
-
-                    vrc_Pickups[13].transform.position = new Vector3(playerPosition.x - 0.2f, playerPosition.y + 3f, playerPosition.z);
-                    vrc_Pickups[14].transform.position = new Vector3(playerPosition.x - 0.4f, playerPosition.y + 3f, playerPosition.z);
-                    vrc_Pickups[25].transform.position = new Vector3(playerPosition.x - 0.6f, playerPosition.y + 3f, playerPosition.z);
-
-                    vrc_Pickups[15].transform.position = new Vector3(playerPosition.x + 0.2f, playerPosition.y, playerPosition.z);
-                    vrc_Pickups[16].transform.position = new Vector3(playerPosition.x + 0.4f, playerPosition.y, playerPosition.z);
-                    vrc_Pickups[26].transform.position = new Vector3(playerPosition.x + 0.6f, playerPosition.y, playerPosition.z);
-
-                }
-                catch { }
             }
         }
 
